3a3b8fc3326540b113558631b98a244ef840529a
Replit user <>
2023-08-25 03:18:28 +0000

    new try

--- list.c
+++ list.c
@@ -59,9 +59,9 @@ void * lastList(List * list) {
     }
     return NULL;
 }
-
+/* retorna el dato del nodo anterior a current y actualiza el current para que apunte a ese nodo. */
 void * prevList(List * list) {
-    if(list->current != NULL && list->current->data != NULL) {
+    if(list->current != NULL && list->current->prev != NULL) {
       list->current = list->current->prev;
       return (list->current->data);
     }
