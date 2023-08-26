f96a157a009986673857b64fb21ce3733d2d3140
Replit user <>
2023-08-26 03:38:13 +0000

    new try

--- list.c
+++ list.c
@@ -133,7 +133,7 @@ void * popCurrent(List * list) {
         list->current->next->prev = list->current->prev;
       }
       list->current = list->current->next;
-      free(list->current->prev);
+      free(list->current);
       list->tail--;
       return dato;
     }
